8a8428d0e3177c390d55b23caa61c8e77f1e3c4a
ridwan16dec <ridwan16dec95@gmail.com>
2026-07-02 15:36:40 +0700

    setup github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['pt-aluminium-kreasi-indonesia.lvh.me', 'pt-aki.lvh.me'],
-  },
+  base: '/Web_AKASIA/',
 })
